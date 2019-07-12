4c54da783762ecf283af1ddd64f08397eb5fec32
Mark Higham <mark.higham@digital.trade.gov.uk>
2019-07-12 15:52:58 +0100

    Govuk styles (#1)
    * starting uk styles
    
    * wip: adding styles
    
    * removed redundant logging output from settings
    
    * front end tooling
    
    * more tooling
    
    * front-end tooling done
    
    * styles fully integrated for static html. perhaps need to tweak for cms
    generated p and div tags

--- helpdesk/helpdesk/settings/base.py
+++ helpdesk/helpdesk/settings/base.py
@@ -155,6 +155,7 @@ STATICFILES_FINDERS = [
 
 STATICFILES_DIRS = [
     os.path.join(PROJECT_DIR, 'static'),
+    os.path.join(BASE_DIR, "frontend"),
 ]
 
 # ManifestStaticFilesStorage is recommended in production, to prevent outdated
