063a5fdb6e28164e5bbfdd135aea7d7f7bcb9087
Mark Higham <mark.higham@digital.trade.gov.uk>
2019-08-02 11:32:51 +0100

    Refactor (#6)
    * removing commented lines
    
    * creating build pipelines

--- app/helpcentre/settings/production.py
+++ app/helpcentre/settings/production.py
@@ -3,6 +3,10 @@ from django.core.exceptions import ImproperlyConfigured
 
 DEBUG = False
 
+SECRET_KEY = env.str('SECRET_KEY')
+
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS')
+
 FEED_API_TOKEN = env.str('FEED_API_TOKEN')
 if not FEED_API_TOKEN:
     raise ImproperlyConfigured('The FEED_API_TOKEN must not be empty')
