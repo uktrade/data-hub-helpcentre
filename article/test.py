6438b6eb53fa1d28a38074054e24315defeeb397
Mark Higham <mark.higham@gmail.com>
2020-02-13 21:25:09 +0000

    renaming tests

--- article/test.py
+++ article/test.py
@@ -5,7 +5,7 @@ from home.models import HomePage
 
 # noinspection PyMethodMayBeStatic
 class ArticleIndexPageTests(WagtailPageTests):
-    def test_can_create_an_article_index_page(self):
+    def test_cannot_create_article_page_under_homepage(self):
         self.assertCanNotCreateAt(HomePage, ArticlePage)
 
     def test_can_create_article_page_index_index_page(self):
