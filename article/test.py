d5a555b405a2de2a74aaaa83799e70d76680ed46
Sam Dudley <samuel.dudley@digital.trade.gov.uk>
2021-03-23 10:12:04 +0000

    Convert accessibility statement migration to a command

--- article/test.py
+++ article/test.py
@@ -1,10 +1,12 @@
+from django.core.management import call_command
+from wagtail.core.models import Page
 from wagtail.tests.utils import WagtailPageTests
+
+from home.models import HomePage
 from .models import (
     ArticlePage,
     ArticleIndexPage,
 )
-
-from home.models import HomePage
 
 
 # noinspection PyMethodMayBeStatic
@@ -14,3 +16,14 @@ class ArticleIndexPageTests(WagtailPageTests):
 
     def test_can_create_article_page_index_index_page(self):
         self.assertCanCreateAt(ArticleIndexPage, ArticlePage)
+
+
+class CreateAccessibilityStatementTests(WagtailPageTests):
+    def test_command_creates_page(self):
+        call_command("create_accessibility_statement")
+
+        home_page = Page.objects.filter(slug="home").first()
+        page = ArticlePage.objects.filter(slug="accessibility-statement").first()
+
+        self.assertTrue(page)
+        self.assertEqual(page.get_parent(), home_page)
