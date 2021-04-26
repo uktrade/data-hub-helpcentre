4e26a5cb0de55f2cd94a131858344f1fb5c9de1c
Ross Miller <github@decodedcollective.com>
2021-04-26 14:51:40 +0100

    Black, update settings

--- config/settings/base.py
+++ config/settings/base.py
@@ -76,7 +76,7 @@ SASS_PROCESSOR_ROOT = os.path.join(BASE_DIR, "/frontend")
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [os.path.join(BASE_DIR, "templates"), ],
+        "DIRS": [os.path.join(BASE_DIR, "templates"),],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
