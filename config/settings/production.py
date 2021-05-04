86423fe78bcf96ad8f8a7d5bd52f8403eb3c2957
Ross Miller <github@decodedcollective.com>
2021-05-04 16:26:16 +0100

    Add audit log middleware

--- config/settings/production.py
+++ config/settings/production.py
@@ -8,4 +8,11 @@ FEED_API_TOKEN = env.str("FEED_API_TOKEN")
 if not FEED_API_TOKEN:
     raise ImproperlyConfigured("The FEED_API_TOKEN must not be empty")
 
-MIDDLEWARE.append("config.middlewares.RedirectDomainMiddleware")
+MIDDLEWARE += [
+    "config.middlewares.RedirectDomainMiddleware",
+    "django_audit_log_middleware.AuditLogMiddleware",
+]
+
+INSTALLED_APPS += [
+    "django_audit_log_middleware",
+]
