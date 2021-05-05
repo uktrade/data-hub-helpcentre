34e7bf4ef74965b31b910e588f26bb32c44a1d46
Ross Miller <github@decodedcollective.com>
2021-05-05 09:59:09 +0100

    LST-1356 Add security audit log middleware

--- config/settings/production.py
+++ config/settings/production.py
@@ -8,4 +8,14 @@ FEED_API_TOKEN = env.str("FEED_API_TOKEN")
 if not FEED_API_TOKEN:
     raise ImproperlyConfigured("The FEED_API_TOKEN must not be empty")
 
-MIDDLEWARE.append("config.middlewares.RedirectDomainMiddleware")
+MIDDLEWARE += [
+    "config.middlewares.RedirectDomainMiddleware",
+    "django_audit_log_middleware.AuditLogMiddleware",
+]
+
+INSTALLED_APPS += [
+    "django_audit_log_middleware",
+]
+
+# Audit log middleware user field
+AUDIT_LOG_USER_FIELD = "username"
