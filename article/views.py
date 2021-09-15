34e595ba76cd4f428e9f70b2284501a1e00506f6
nboyse <natasha.boyse@digital.trade.gov.uk>
2021-09-15 17:43:06 +0100

    LST-1452 - Add proper token authentication to Data Hub Help Centre (data lake access)

--- article/views.py
+++ article/views.py
@@ -1,57 +1,50 @@
 import logging
 
-from django.http import JsonResponse
-from django.conf import settings
-from django.core.exceptions import PermissionDenied
-
 from wagtail.core.models import Site
 
 from .models import ArticlePage
 
+from .hawk import HawkAuthentication
+
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
 logger = logging.getLogger(__name__)
 
+class ChildArticleFeedView(APIView):
+    authentication_classes = (HawkAuthentication,)
 
-def child_article_feed(request, path):
-    logger.debug(path)
-    path_components = [component for component in path.split("/") if component]
-    logger.debug(f"components {path_components}")
+    def get(self, request, path, format=None):
+        logger.debug(path)
+        path_components = [component for component in path.split("/") if component]
+        logger.debug(f"components {path_components}")
 
-    page = (
-        Site.find_for_request(request)
-        .root_page.specific.route(request, path_components)
-        .page.specific
-    )
-    logger.debug(page)
+        page = (
+            Site.find_for_request(request)
+            .root_page.specific.route(request, path_components)
+            .page.specific
+        )
+        logger.debug(page)
 
-    limit_query = request.GET.get("limit", 3)
-    if settings.DEBUG:
-        logger.warning("Not checking for API token")
-    else:
-        expected_token = f"Bearer {settings.FEED_API_TOKEN}"
-        auth_token = request.META.get("HTTP_AUTHORIZATION", "")
+        limit_query = request.GET.get("limit", 3)
+        limit = int(limit_query)
+        logger.debug(f"limit: {limit}")
 
-        if not expected_token == auth_token:
-            logger.error(f"Bad api token {auth_token}")
-            raise PermissionDenied()
+        recent_articles = ArticlePage.objects.live().descendant_of(page).order_by("-date")[:limit]
+        site_root = Site.find_for_request(request).root_url
 
-    limit = int(limit_query)
-    logger.debug(f"limit: {limit}")
+        count = len(recent_articles)
+        logger.debug(f"found {count} articles for {path}")
 
-    recent_articles = ArticlePage.objects.live().descendant_of(page).order_by("-date")[:limit]
-    site_root = Site.find_for_request(request).root_url
+        result = {"count": len(recent_articles), "articles": []}
 
-    count = len(recent_articles)
-    logger.debug(f"found {count} articles for {path}")
+        for recent in recent_articles:
+            path = f"{site_root}{recent.url}"
+            article = {
+                "html_url": path,
+                "title": recent.title,
+                "created_at": recent.date,
+            }
+            result["articles"].append(article)
 
-    result = {"count": len(recent_articles), "articles": []}
-
-    for recent in recent_articles:
-        path = f"{site_root}{recent.url}"
-        article = {
-            "html_url": path,
-            "title": recent.title,
-            "created_at": recent.date,
-        }
-        result["articles"].append(article)
-
-    return JsonResponse(result)
+        return Response(result)
