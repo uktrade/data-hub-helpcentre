b8688fbb1c8073603bba8d4d8d826760a0cc9694
Nawaz <nawaz.ahmed@digital.trade.gov.uk>
2022-05-03 13:23:32 +0100

    black made changes to autogenerated files

--- article/migrations/0013_alter_articlepage_body.py
+++ article/migrations/0013_alter_articlepage_body.py
@@ -10,13 +10,66 @@ import wagtail.images.blocks
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('article', '0012_auto_20210420_1629'),
+        ("article", "0012_auto_20210420_1629"),
     ]
 
     operations = [
         migrations.AlterField(
-            model_name='articlepage',
-            name='body',
-            field=wagtail.core.fields.StreamField([('paragraph', wagtail.core.blocks.RichTextBlock()), ('image', wagtail.images.blocks.ImageChooserBlock()), ('embed_video', wagtail.core.blocks.StructBlock([('video', wagtail.embeds.blocks.EmbedBlock(help_text='Paste a URL from Microsoft Stream or Youtube. Please use the page URL rather than the URL from the embed code.'))], help_text='Embed a video')), ('code', wagtail.core.blocks.StructBlock([('language', wagtail.core.blocks.ChoiceBlock(choices=[('bash', 'Bash/Shell'), ('css', 'CSS'), ('diff', 'diff'), ('html', 'HTML'), ('javascript', 'Javascript'), ('json', 'JSON'), ('python', 'Python'), ('r', 'R'), ('scss', 'SCSS'), ('yaml', 'YAML')], help_text='Coding language', identifier='language', label='Language')), ('code', wagtail.core.blocks.TextBlock(identifier='code', label='Code'))], label='Code'))], blank=True, null=True),
+            model_name="articlepage",
+            name="body",
+            field=wagtail.core.fields.StreamField(
+                [
+                    ("paragraph", wagtail.core.blocks.RichTextBlock()),
+                    ("image", wagtail.images.blocks.ImageChooserBlock()),
+                    (
+                        "embed_video",
+                        wagtail.core.blocks.StructBlock(
+                            [
+                                (
+                                    "video",
+                                    wagtail.embeds.blocks.EmbedBlock(
+                                        help_text="Paste a URL from Microsoft Stream or Youtube. Please use the page URL rather than the URL from the embed code."
+                                    ),
+                                )
+                            ],
+                            help_text="Embed a video",
+                        ),
+                    ),
+                    (
+                        "code",
+                        wagtail.core.blocks.StructBlock(
+                            [
+                                (
+                                    "language",
+                                    wagtail.core.blocks.ChoiceBlock(
+                                        choices=[
+                                            ("bash", "Bash/Shell"),
+                                            ("css", "CSS"),
+                                            ("diff", "diff"),
+                                            ("html", "HTML"),
+                                            ("javascript", "Javascript"),
+                                            ("json", "JSON"),
+                                            ("python", "Python"),
+                                            ("r", "R"),
+                                            ("scss", "SCSS"),
+                                            ("yaml", "YAML"),
+                                        ],
+                                        help_text="Coding language",
+                                        identifier="language",
+                                        label="Language",
+                                    ),
+                                ),
+                                (
+                                    "code",
+                                    wagtail.core.blocks.TextBlock(identifier="code", label="Code"),
+                                ),
+                            ],
+                            label="Code",
+                        ),
+                    ),
+                ],
+                blank=True,
+                null=True,
+            ),
         ),
     ]
