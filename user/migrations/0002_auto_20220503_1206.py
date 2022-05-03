1beb3ea7fbec163d80523700d1a25d416121732f
Nawaz <nawaz.ahmed@digital.trade.gov.uk>
2022-05-03 13:23:32 +0100

    black made changes to autogenerated files

--- user/migrations/0002_auto_20220503_1206.py
+++ user/migrations/0002_auto_20220503_1206.py
@@ -6,18 +6,18 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('user', '0001_initial'),
+        ("user", "0001_initial"),
     ]
 
     operations = [
         migrations.AlterField(
-            model_name='historicaluser',
-            name='first_name',
-            field=models.CharField(blank=True, max_length=150, verbose_name='first name'),
+            model_name="historicaluser",
+            name="first_name",
+            field=models.CharField(blank=True, max_length=150, verbose_name="first name"),
         ),
         migrations.AlterField(
-            model_name='user',
-            name='first_name',
-            field=models.CharField(blank=True, max_length=150, verbose_name='first name'),
+            model_name="user",
+            name="first_name",
+            field=models.CharField(blank=True, max_length=150, verbose_name="first name"),
         ),
     ]
