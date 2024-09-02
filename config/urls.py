f6651fcb33ea7d9444323835ba052d6a7ecd8e82
David Okeke <david.okeke@madetech.com>
2024-09-02 14:25:49 +0100

    Refactored pingdom healthcheck endpoint

--- config/urls.py
+++ config/urls.py
@@ -9,7 +9,6 @@ from wagtail.documents import urls as wagtaildocs_urls
 from authbroker_client import urls as authbroker_client_urls
 
 from article import urls as article_urls
-from config.views_healthcheck import health_check_view
 
 from search import views as search_views
 
@@ -19,7 +18,7 @@ urlpatterns = [
     path("documents/", include(wagtaildocs_urls)),
     path("search/", search_views.search, name="search"),
     path("auth/", include(authbroker_client_urls)),
-    path("check/", health_check_view, name="healthcheck_view"),
+    path("pingdom/", include("pingdom.urls")),
     path("api/feeds/", include(article_urls)),
     # For anything not caught by a more specific rule above, hand over to
     # Wagtail's page serving mechanism. This should be the last pattern in
