d6c207a0bb76df3d43f4a99ee416c2cbe4c86689
jamesprobinson <james.robinson@businessandtrade.gov.uk>
2024-09-02 16:05:50 +0100

    tidy up with aster

--- article/urls.py
+++ article/urls.py
@@ -1,15 +1,7 @@
 from django.urls import path
 
-from .views import ChildArticleFeedView, InlineFeedBackViewSet
+from .views import ChildArticleFeedView
 
 urlpatterns = [
     path("<path:path>", ChildArticleFeedView.as_view(), name="child_article_feed"),
-    path(
-        "submit_feedback", InlineFeedBackViewSet.as_view({"post": "create"}), name="submit_feedback"
-    ),
-    path(
-        "submit_feedback",
-        InlineFeedBackViewSet.as_view({"patch": "partial_update"}),
-        name="update",
-    ),
 ]
