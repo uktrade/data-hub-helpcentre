bc05a9d5ec0495354641c305e8901f391c2f15c2
James Robinson <70377025+JamesPRobinson@users.noreply.github.com>
2024-10-22 09:10:53 +0100

    add ignore path for sso client and inline feedback route

--- config/settings/base.py
+++ config/settings/base.py
@@ -249,7 +249,11 @@ WAGTAILEMBEDS_FINDERS = [
     }
 ]
 
-AUTHBROKER_ANONYMOUS_PATHS = ("check", "/api/feeds/data-hub/updates/")
+AUTHBROKER_ANONYMOUS_PATHS = (
+    "check",
+    "/api/feeds/data-hub/updates/",
+    "/api/pipeline/user-inline-feedback-survey",
+)
 
 LOGGING = {
     "version": 1,
