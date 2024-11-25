9f67298596a47bee09fa447011cabe79f32d77eb
Jahir <jahir.uddin@digital.trade.gov.uk>
2024-11-25 15:04:53 +0000

    removed static env value from the prebuild

--- .copilot/image_build_run.sh
+++ .copilot/image_build_run.sh
@@ -2,5 +2,6 @@
 
 # Exit early if something goes wrong
 set -e
-
+python manage.py compilescss
+python manage.py collectstatic --noinput
 # Add commands below to run inside the container after all the other buildpacks have been applied
