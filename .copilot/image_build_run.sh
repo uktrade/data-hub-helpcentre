dd1e29af8ea2b87d43f6194117cd977574c01048
Ravi Pradhan <ravi.pradhan@digital.trade.gov.uk>
2024-11-26 15:03:57 +0000

    add explicit copilot env name to see if this leads to a successful codebuild job

--- .copilot/image_build_run.sh
+++ .copilot/image_build_run.sh
@@ -3,6 +3,8 @@
 # Exit early if something goes wrong
 set -e
 
+export COPILOT_ENVIRONMENT_NAME=nonprod
+
 # Add commands below to run inside the container after all the other buildpacks have been applied
 python manage.py compilescss
 python manage.py collectstatic --noinput
