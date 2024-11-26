30da0fb629b447bf3484d30f3a52686377d0fb4e
Ravi Pradhan <ravi.pradhan@digital.trade.gov.uk>
2024-11-26 12:02:15 +0000

    Merge with main and update image build run with static asset compilation

--- .copilot/image_build_run.sh
+++ .copilot/image_build_run.sh
@@ -17,3 +17,5 @@ export DEBUG="True"
 python manage.py compilescss
 python manage.py collectstatic --noinput
 # Add commands below to run inside the container after all the other buildpacks have been applied
+python manage.py compilescss
+python manage.py collectstatic --noinput
