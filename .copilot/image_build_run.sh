823a9f856dc8165f270d66428806ac794bd45273
Jahir <jahir.uddin@digital.trade.gov.uk>
2024-11-27 10:15:26 +0000

    exporting the env in the image build

--- .copilot/image_build_run.sh
+++ .copilot/image_build_run.sh
@@ -2,6 +2,7 @@
 
 # Exit early if something goes wrong
 set -e
+export $(grep -v '^#' sample.env | xargs)
 python manage.py compilescss
 python manage.py collectstatic --noinput
 # Add commands below to run inside the container after all the other buildpacks have been applied
