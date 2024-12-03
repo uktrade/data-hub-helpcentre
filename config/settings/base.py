1ced668b00f0a3d98e94b63b8af147dcf5a2aa69
Ravi Pradhan <ravi.pradhan@digital.trade.gov.uk>
2024-12-03 16:02:54 +0000

    try changing redirect URL to named value

--- config/settings/base.py
+++ config/settings/base.py
@@ -74,7 +74,7 @@ AUTHENTICATION_BACKENDS = [
 ]
 
 LOGIN_URL = reverse_lazy("authbroker_client:login")
-LOGIN_REDIRECT_URL = "/"
+LOGIN_REDIRECT_URL = "search"
 ROOT_URLCONF = "config.urls"
 
 WAGTAIL_FRONTEND_LOGIN_URL = reverse_lazy("authbroker_client:login")
