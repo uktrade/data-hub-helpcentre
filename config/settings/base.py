40f1d1ed6d9379b3dd82e48cde50a9769750f3a5
Ian leggett <>
2024-12-23 15:06:53 +0000

    fix: conditionally set s3 bucket paths for both envs

--- config/settings/base.py
+++ config/settings/base.py
@@ -174,6 +174,9 @@ AWS_DEFAULT_ACL = None
 DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
 STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
 
+if not is_copilot():
+    AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com"
+
 
 if not is_copilot():
     # Only required in Cloud Foundry.
